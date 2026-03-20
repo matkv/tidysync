a67b10627cb1ea4e06c03b769db4118ada894582
matkv <mail@matkv.dev>
2026-03-20 10:09:04 +0100

    call ping command in main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+use anyhow::Result;
+use clap::Parser;
+
+use crate::{cli::CLI, client::SyncThingClient};
+
 mod cli;
 mod client;
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = CLI::parse();
+    let syncthing = SyncThingClient::new(args.url, args.api_key);
+
+    match args.command {
+        cli::Command::Ping => {
+            syncthing.ping().await?;
+            println!("Syncthing is responsive!");
+        }
+    }
+    Ok(())
 }
