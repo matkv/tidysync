6c01b3e11ddde77f31c38c223349be36de448d77
matkv <mail@matkv.dev>
2026-03-20 09:59:46 +0100

    add cli struct

---
+++ src/cli.rs
@@ -0,0 +1,22 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(
+    name = "tidysync",
+    version,
+    about = "A CLI for cleaning up files synced with Syncthing"
+)]
+
+pub struct CLI {
+    #[arg(short, long, default_value = "http://localhost:8384")]
+    pub url: String,
+    #[arg(long, env = "SYNCTHING_API_KEY")]
+    pub api_key: String,
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Subcommand)]
+pub enum Command {
+    Ping,
+}
