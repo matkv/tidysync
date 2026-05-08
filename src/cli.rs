9bb680c46c5e832a675cdfaba82febb6ce07c2cd
matkv <mail@matkv.dev>
2026-05-08 11:08:29 +0200

    add tray mode flag

--- src/cli.rs
+++ src/cli.rs
@@ -16,6 +16,8 @@ pub struct CLI {
     pub api_key: Option<String>,
     #[arg(long, value_name = "CONFIG_PATH")]
     pub config: Option<PathBuf>,
+    #[arg(long, help = "Run in system tray mode")]
+    pub tray: bool,
     #[command(subcommand)]
     pub command: Option<Command>,
 }
